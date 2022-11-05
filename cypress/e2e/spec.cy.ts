279d8d97d1fd6d6a2d836681aac92738f6d7257e
gulberkdemir <62015829+gulberkdemir@users.noreply.github.com>
2022-11-05 14:50:47 +0300

    project is done

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,4 +1,5 @@
 let desctext: any;
+let humidity: any;
 describe('E2E TEST FOR WEATHER-APP', () => {
   before(() => {
     cy.visit('/')
@@ -20,18 +21,31 @@ describe('E2E TEST FOR WEATHER-APP', () => {
 
   })
 
-  it('Check "Description" Network Results', () => {
+  it('Check "Description" and "Humidity" Network Results', () => {
     cy.get('.city-card__city-name .city-card__sky-state').then(($btn) => {
       // redefine text reference
       let text = $btn.text()
       desctext = text;
     })
 
-    cy.request('https://api.openweathermap.org/data/2.5/weather?appid=0f3fb9fa31ad3d41f1bb2bd0841c3f2f&q=istanbul&units=metric').as('comments')
+    cy.get('.city-card__info .city-card__info-list .city-card__info-list-item:nth-child(2) span').then(($btn) => {
+      // redefine text reference
+      let text2 = $btn.text()
+      humidity = text2;
+      humidity = humidity.slice(0, -1);
+      humidity= +humidity;
+
+    })
+
+    cy.request('https://api.openweathermap.org/data/2.5/weather?appid=0f3fb9fa31ad3d41f1bb2bd0841c3f2f&q=istanbul&units=metric')
+      .as('comments')
     cy.get('@comments').should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).to.have.property('name', 'Istanbul')
       expect(response.body.weather[0]).to.have.property('description', desctext.trim());
+      expect(response.body.main).to.have.property('humidity', humidity);
     })
   })
+
+
 })
